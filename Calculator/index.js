b2bd7597feb3d19642d82b6dfeb68bbd46aa8d18
vijaya <vijayalaxmistorage@gmail.com>
2026-06-13 17:28:31 +0530

    first commit

---
+++ Calculator/index.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("input-container");
+
+let calculated = false;
+
+function appendValue(value) {
+
+    if (calculated) {
+        display.value = value;
+        calculated = false;
+    } else {
+        display.value += value;
+    }
+    
+
+}
+
+function clearDisplay() {
+    display.value = "";
+    calculated=false;
+    
+}
+
+function Calculate() {
+    display.value = eval(display.value);
+    calculated = true;
+}
+function backSpace(){
+    display.value= display.value.slice(0,-1);
+}
